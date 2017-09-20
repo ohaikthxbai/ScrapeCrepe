066b2019cb51a4832a2a1d4dc5c75f92c1a6f9f7
Justin G <ctuniseji@gmail.com>
2017-09-20 12:09:08 -0500

    setting up the server

---
+++ server.js
@@ -0,0 +1,24 @@
+// require npm packages
+var express = require("express");
+var exphb = require("express-handlebars");
+var mongoodse = require("mongoose");
+var bodyParser = require("body-parser");
+var cheerio = require("cheerio");
+var request = require("request");
+
+// port setup
+var PORT = process.env.PORT || 3000; 
+
+// initialize express
+var app = express();
+
+var router = express.Router();
+
+// saving space for more content here
+
+
+
+app.listen(PORT, function() {
+    // es6 woot!
+    console.log(`Listening on port: ${PORT}`);
+});
