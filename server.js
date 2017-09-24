4975e9e7f750039758bd467fe10b691aab92d639
Justin G <ctuniseji@gmail.com>
2017-09-23 20:37:18 -0500

    various connections to server-js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 // require npm packages
 var express = require("express");
 var exphb = require("express-handlebars");
-var mongoodse = require("mongoose");
+var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
 //var cheerio = require("cheerio");
 //var request = require("request");
@@ -12,16 +12,39 @@ var PORT = process.env.PORT || 3000;
 // initialize express
 var app = express();
 
+// views directory
+app.use(express.static(__dirname + "/public"));
+
+// connecting the handlebars
+app.engine("handlebars", exphb({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+// setting up the express router
 var router = express.Router();
 
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
 
-//requiring routes
+app.use(router);
+
+// requiring routes
 require("./config/routes")(router);
 
+// connect to mongoooose!
+var db = process.env.MONGO_URI || "mongodb://localhost/mongoHeadlines";
 
-// saving space for more content here
-
-
+mongoose.connect(db, function(err) {
+    if (err) {
+        console.log(err);
+    }
+    else {
+        console.log("mongoose connected");
+    }
+});
 
 app.listen(PORT, function() {
     // es6 woot!
