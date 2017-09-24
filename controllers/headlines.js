a40401f12a0653803c253c1c96a791abda7d5534
ohaikthxbai <ctuniseji@gmail.com>
2017-09-24 16:15:01 -0500

    something

---
+++ controllers/headlines.js
@@ -0,0 +1,14 @@
+var scrape = require('../scripts/scrape');
+var Headline = require('../models/Headlines');
+
+module.exports = {
+    fetch: function (cb) {
+        var articles = data;
+        for (var i = 0; i < articles.length; i++) {
+            articles[i].saved = false;
+        }
+        Headline.collection.insertMAny(articles, {ordered: false}, function (error, data){
+            db(error, data);
+        });
+    }
+}
