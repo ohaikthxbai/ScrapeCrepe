db4c57b2aa241fb8af6ee7e97309406c1a6bbc75
ohaikthxbai <ctuniseji@gmail.com>
2017-09-24 16:30:19 -0500

    scrape js created and edited

---
+++ scripts/scrape.js
@@ -0,0 +1,29 @@
+var request = require('request');
+var cheerio = request('cheerio');
+
+var scrape = function (cb) {
+    request("http://nytimes.com", function (error, res, body) {
+        var $ = cherrio.load(body);
+        var articles = [];
+
+         $('.summary').each(function (i, element) {
+             var heading = $(this).children('.article-heding').text().trim;
+             var summary = $(this).children('.summary').text.trim;
+
+             if (head && sum) {
+                 var prettyHead = head.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+                 var prettySum = summary.replace(/(\r\n|\n|\r|\t|\s+)/gm, " ").trim();
+
+                 var temp = {
+                     headline: prettyHead,
+                     summary: prettySum
+                 };
+
+                 articles.push(temp);
+             }
+        });
+        cb(articles);
+    });
+};
+
+module.exports = scrape;
